a9dd36dafe5b5b73cf3364a362668cb23e80f0f8
Marcos <marcosrff15@gmail.com>
2022-07-17 00:49:57 -0300

    Importando classes e adicionando menu inicial

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-pass
+from usuarios import Usuario, Admin, PessoaFisica, PessoaJuridica
+from tenis_carrinho import Tenis, CarrinhoDeCompras
+
+contas = [] #Lista de contas
+admin = Admin('admin01', 'Marcos', '20', 'marcos.rff@grad,ufsc.br')
+
+
+while True:
+    print("1. Cadastrar Usuario")
+    print("2. Atualizar Dados")
+    print("3. Realizar Compra")
+    print("4. Menu Admin")
+    print("5. Sair")
+    op = int(input("Digite um número para executar uma operação: "))
+
+
+    if op == 1:
+        pass
+
+    elif op == 4:
+        print("1. Cadastrar Produto")
+        print("2. Alterar dados do produto")
+        print("3. Excluir Produto")
+        print("4. Excluir Usuário")
+        print("5. Voltar ao menu principal")
