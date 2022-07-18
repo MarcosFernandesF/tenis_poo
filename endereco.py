93c4178aa469838ea677f0f8f4730506d7d0cbb9
Marcos Roberto Fernandes Filho <marcos.rff@ufsc.br>
2022-07-18 20:26:47 -0300

    Novo arquivo para a classe Endereço

---
+++ endereco.py
@@ -0,0 +1,25 @@
+class Endereço():
+    def __init__ (self, rua, complemento, cidade, cep):
+        self.rua = rua
+        self.complemento = complemento
+        self.cidade = cidade
+        self.CEP = cep
+
+    #Atualizar Endereço, função pronta ou sets?
+    def atualizar_endereço(self, rua, complemento, cidade, cep):
+        self.rua = rua
+        self.complemento = complemento
+        self.cidade = cidade
+        self.CEP
+
+    '''def set_rua(self, rua):
+        self.rua = rua
+
+    def set_complemento(self, complemento):
+        self.complemento = complemento
+
+    def set_cidade(self, cidade):
+        self.cidade = cidade
+    
+    def set_CEP(self, cep):
+        self.CEP = cep'''
