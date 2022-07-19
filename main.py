8a758271a62263e45435c9cf16ad487c92b859d0
Marcos <marcosrff15@gmail.com>
2022-07-18 23:34:07 -0300

    Terminando menu admin e 2 e 3 do menu principal

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from usuarios import Usuario, Admin, PessoaFisica, PessoaJuridica
 from tenis_carrinho import Tenis, CarrinhoDeCompras
-from endereço import Endereço
+from endereco import Endereço
 
-admin = Admin('#01', 'Marcos', '20', 'marcos.rff@grad,ufsc.br')
-usuario_cpf = PessoaFisica('#02', 'MarcosCPF', '21', 'marcosrff.2001@gmail.com')
-usuario_cnpj = PessoaJuridica('#03', 'MarcosCNPJ', 'marcos.empresa@gmail.com')
+endereco = Endereço("Lauro Linhares", "Não sei", "Florianópolis", "88036-000")
+admin = Admin(20, 'Marcos', 20, 'marcos.rff@grad,ufsc.br')
+usuario_cpf = PessoaFisica(21, '128.991.219-01', 'MarcosCPF', 21, 'marcosrff.2001@gmail.com', endereco)
+usuario_cnpj = PessoaJuridica(22, '83.899.526/0001-82', 'MarcosCNPJ', 22, 'marcos.empresa@gmail.com')
 
 lista_usuarios = [] #Lista de usuarios
 lista_produtos = [] #Lista de produtos
@@ -12,13 +13,17 @@ estoque = {} #Estoque, dicionario
 id = 0 # ID do usuário
 id_tenis = 0 # ID do tenis
 
-while True:
+lista_usuarios.append(admin)
+lista_usuarios.append(usuario_cpf)
+lista_usuarios.append(usuario_cnpj)
 
+while True:
+    print("-----------------Menu Principal------------------")
     print("1. Cadastrar Usuario")
-    print("2. Atualizar Dados") #Descobrir como diferenciar pessoa fisica de juridica através da classe
-    print("3. Atualizar Endereço") #Descobrir como diferenciar pessoa fisica de juridica através da classe
+    print("2. Atualizar Dados") 
+    print("3. Atualizar Endereço") #É preciso listar o endereço em algum momento
     print("4. Realizar Compra") 
-    print("5. Menu Admin") #Op == 3 ERRO
+    print("5. Menu Admin")
     print("6. Sair")
     op = int(input("Digite um número para executar uma operação: "))
     print()
@@ -62,78 +67,128 @@ while True:
 
     elif op == 2:
         id = int(input("Deseja atualizar os dados de qual conta? Digite o ID: "))
-        for posicao,dados in enumerate(lista_usuarios):
+        pessoa = input("Pessoa Física ou Jurídica? [F/J]: ").upper()
+
+        for posicao, dados in enumerate(lista_usuarios):
             if id == dados.get_id():
-                pass
-                #Descobrir como diferenciar pessoa fisica de juridica através da classe
-            else:
-                print("Essa conta não existe!")
-                print("")
+                email = input("Email: ")
+                nome = input("Nome: ")
+                idade = int(input("Idade: "))
+
+                if pessoa == 'F':
+                    cpf = input("CPF: ")
+                    usuario = dados.atualizar_dados(cpf, nome, idade, email)
+                    print("Dados atualizados com sucesso!")
+                    break
+                elif pessoa == 'J':
+                    cnpj = input("CNPJ: ")
+                    usuario = dados.atualizar_dados(cnpj, nome, idade, email)
+                    print("Dados atualizados com sucesso!")
+                    break
+        else:
+            print("A conta não existe!")
+
     
     elif op == 3:
         id = int(input("Digite o ID da conta que deseja mudar o endereço: "))
+        pessoa = input("Pessoa Física ou Jurídica? [F/J]: ").upper()
+
         for posicao, dados in enumerate(lista_usuarios):
             if id == dados.get_id():
-                pass
-                #Descobrir como diferenciar pessoa fisica de juridica através da classe
+                if pessoa == 'F':
+                    rua = input("Rua: ")
+                    complemento = input("Complemento: ")
+                    cidade = input("Cidade: ")
+                    cep = input("CEP: ")
+
+                    endereço = dados.endereço.atualizar_endereço(rua, complemento, cidade, cep)
+                    print("Endereço atualizado com sucesso!")
+                    print("")
+                    break
+                elif pessoa == 'J':
+                    print("Apenas pessoas físicas possuem endereço ")
+                    print("")
+                    break
             else:
                 print("Essa conta não existe!")
                 print("")
     
     elif op == 5:
-        print("Menu ADMIN")
-        print("1. Cadastrar Produto")
-        print("2. Alterar dados do produto")
-        print("3. Excluir Produto")
-        print("4. Excluir Usuário")
-        print("5. Voltar ao menu principal")
-        op = int(input("Digite um número para executar uma operação: "))
-        print("")
-
-        if op == 1:
-            id_tenis += 1
-            nome = input("Nome do produto: ")
-            quantidade = int(input("Quantidade: "))
-            valor = float(input("Preço: "))
+        while True: #Laço para continuar no mesmo menu mesmo após terminar a operação
+            print("--------------------Menu ADMIN--------------------")
+            print("1. Cadastrar Produto")
+            print("2. Alterar dados do produto")
+            print("3. Excluir Produto")
+            print("4. Excluir Usuário")
+            print("5. Voltar ao menu principal")
+            op = int(input("Digite um número para executar uma operação: "))
             print("")
-            
-            produto = Tenis(id_tenis, nome, quantidade, valor)
-            lista_produtos.append(produto)
 
-            if nome not in estoque:
-                estoque[nome] = quantidade
+            if op == 1:
+                id_tenis += 1
+                nome = input("Nome do produto: ")
+                quantidade = int(input("Quantidade: "))
+                valor = float(input("Preço: "))
+                print("")
+                
+                produto = Tenis(id_tenis, nome, quantidade, valor)
+                lista_produtos.append(produto)
 
-        elif op == 2:
-            for i in range (len(lista_produtos)):
-                lista_produtos[i].listar_dados()
-            
-            print("")
-            id_produto = int(input("Digite o ID do produto que deseja modificar: "))
+                if nome not in estoque:
+                    estoque[nome] = quantidade
 
-            for posicao, dados in enumerate(lista_produtos):
-                if id_produto == dados.get_id():
-                    nome_antigo = dados.get_nome()
-                    nome = input("Nome do produto: ")
-                    quantidade = int(input("Quantidade: "))
-                    valor = float(input("Preço: "))
+            elif op == 2:
+                for i in range (len(lista_produtos)):
+                    lista_produtos[i].listar_dados()
+                
+                print("")
+                id_produto = int(input("Digite o ID do produto que deseja modificar: "))
 
-                    estoque[nome_antigo] = quantidade
-                    lista_produtos[posicao].atualizar_dados(nome,quantidade,valor)
-                    print("Produto alterado com sucesso!")
-                    print("")
-                else:
-                    print("Esse produto não existe ")
+                for posicao, dados in enumerate(lista_produtos):
+                    if id_produto == dados.get_id():
+                        nome_antigo = dados.get_nome()
+                        nome = input("Nome do produto: ")
+                        quantidade = int(input("Quantidade: "))
+                        valor = float(input("Preço: "))
 
-        elif op == 3:
-            for i in range (len(lista_produtos)):
-                lista_produtos[i].listar_dados()
-            
-            print("")
-            id_produto = int(input("Digite o ID do produto que deseja excluir: "))
-            for posicao, dados in enumerate(lista_produtos):
-                if id_produto == dados.get_id():
-                    print(estoque.pop(dados.get_nome(), "Produto não encontrado"))
-                    lista_produtos[posicao].remove(dados) #Função errada, erro compilação
-                    print("Produto excluído com sucesso!")
-                    print("")
+                        estoque[nome_antigo] = quantidade
+                        lista_produtos[posicao].atualizar_dados(nome,quantidade,valor)
+                        print("Produto alterado com sucesso!")
+                        print("")
+                    else:
+                        print("Esse produto não existe ")
 
+            elif op == 3:
+                for i in range (len(lista_produtos)):
+                    lista_produtos[i].listar_dados()
+                
+                print("")
+
+                id_produto = int(input("Digite o ID do produto que deseja excluir: "))
+                for posicao, dados in enumerate(lista_produtos):
+                    if id_produto == dados.get_id():
+                        print(estoque.pop(dados.get_nome(), "Produto não encontrado"))
+                        lista_produtos.remove(dados)
+
+                        print("Produto excluído com sucesso!")
+                        print("")
+
+            elif op == 4:
+                for i in range (len(lista_usuarios)):
+                    lista_usuarios[i].listar_dados()
+        
+                print("")
+
+                id = int(input("Digite o ID do usuario que deseja excluir: "))
+                for posicao, dados in enumerate(lista_usuarios):
+                    if id == dados.get_id():
+                        lista_usuarios.remove(dados)
+                        print("Usuário excluído com sucesso!")
+                        print("")
+
+            elif op == 5:
+                break
+
+            else:
+                print("Numero inválido")
+                print("")
